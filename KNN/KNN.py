e70afe1b54141afb77eb080a4a54f1b7f5ab9e5a
Hoang Viet Tung <132819787+tyl1312@users.noreply.github.com>
2024-05-31 21:06:53 +0700

    Update KNN.py

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,13 +1,8 @@
 import pickle
-import os
 
 def knn_model():
 
-    current_dir = os.path.dirname(__file__)
-    
-    pickle_file_path = os.path.join(current_dir, 'KNN_model.pkl')
-
-    with open(pickle_file_path, 'rb') as file:
+    with open('KNN_model.pkl', 'rb') as file:
         model = pickle.load(file)
     return model
 
